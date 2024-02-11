66ddcfbc96549c21d328bc842363168a46c27d1a
Shekhri <khamidovashakhrezada3868@gmail.com>
2024-02-12 02:07:39 +0500

    add

---
+++ js/MockData.js
@@ -0,0 +1,39 @@
+var data = [
+    {
+        id: 1,
+        image: './assets/images/box1.png',
+        Name: '@Alex',
+        price: '0.005 ETH'
+    },
+    {
+        id: 1,
+        image: './assets/images/box2.png',
+        Name: '@Vorya',
+        price: '0.006 ETH'
+    },
+    {
+        id: 1,
+        image: './assets/images/box3.png',
+        Name: '@Mira',
+        price: '0.007 ETH'
+    },
+    {
+        id: 1,
+        image: './assets/images/box4.png',
+        Name: '@Nikita',
+        price: '0.008 ETH'
+    },
+    {
+        id: 1,
+        image: './assets/images/box5.png',
+        Name: '@Sasha',
+        price: '0.009 ETH'
+    },
+    {
+        id: 1,
+        image: './assets/images/box6.png',
+        Name: '@Marat',
+        price: '0.010 ETH'
+    },
+   
+]
